e4bfb94db2f97b3cec5c01b744746240d1e3389f
CamiloAlbertoOspinaAgudelo <148649416+CamiloAlbertoOspinaAgudelo@users.noreply.github.com>
2025-09-16 09:08:23 -0500

    dto1

--- src/main/java/co/edu/uniquindio/application/controllers/UserController.java
+++ src/main/java/co/edu/uniquindio/application/controllers/UserController.java
@@ -1,26 +1,36 @@
 package co.edu.uniquindio.application.controllers;
 
+import co.edu.uniquindio.application.dto.CreateHostDTO;
 import co.edu.uniquindio.application.dto.CreateUserDTO;
 import co.edu.uniquindio.application.dto.EditUserDTO;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import co.edu.uniquindio.application.dto.UserDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+    @PostMapping
     public void create(CreateUserDTO userDTO) throws Exception{
     }
 
+    @PutMapping
     public void edit(EditUserDTO userDTO) throws Exception{
     }
 
+    @DeleteMapping("/{id}")
     public void delete(String id) throws Exception{
     }
 
+    @GetMapping("/{id}")
     public UserDTO get(String id) throws Exception{
+        return null;
     }
 
-    public List<UserDTO> listAll(){
+    @PostMapping("/host")
+    public void createHost(CreateHostDTO hostDTO) throws Exception{
+
     }
 }
